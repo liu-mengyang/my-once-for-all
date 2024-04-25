3e0ca9c73484c3108f9b1999e9b00814809271e9
liu-mengyang <myliu@seu.edu.cn>
2024-04-25 14:00:44 +0000

    Add extraction script

---
+++ extract.sh
@@ -0,0 +1,4 @@
+python extract_ofa_net.py -g 0 -n ofa_resnet50 -ns 1000 > resnet50_ofa_extraction_log.txt
+python extract_ofa_net.py -g 0 -n ofa_mbv3_d234_e346_k357_w1.0 -ns 1000 > mbv3w10_ofa_extraction_log.txt
+python extract_ofa_net.py -g 0 -n ofa_mbv3_d234_e346_k357_w1.2 -ns 1000 > mbv3w12_ofa_extraction_log.txt
+python extract_ofa_net.py -g 0 -n ofa_proxyless_d234_e346_k357_w1.3 -ns 1000 > proxylessnet_ofa_extraction_log.txt
